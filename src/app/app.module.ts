b86c61fb4fc4520ab513c84344f65617b67503bf
Rico dos Santos <rico.dos.santos@mheducation.com>
2018-12-17 11:11:24 -0500

    add ng-packagr and version bump

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { DatagridModule } from './lib/datagrid/datagrid.module';
+import { DatagridModule } from '../lib/ngx-datagrid.module';
 
 enableProdMode();
 @NgModule({
